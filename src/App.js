2f8fba6c40549ac30cfc76ad6b010d5a182a3343
WebDeverDan <danielbvideo@gmail.com>
2025-11-15 10:27:09 -0500

    added code for better page loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import styled from "styled-components";
+import { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 import ProfComponent from "./components/Banner/ProfComponent";
 import Skills from "./components/Skills/Skills";
 import Footer from "./components/Footer/Footer";
 import Projects from "./components/Projects/Projects";
 import Services from "./components/Service/Services";
+
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const images = Array.from(document.images);
+    const promises = images.map(img =>
+      img.complete ? Promise.resolve() :
+      new Promise(resolve => {
+        img.onload = img.onerror = resolve;
+      })
+    );
+    Promise.all(promises).then(() => setTimeout(() => setIsLoaded(true), 100));
+  }, []);
+
   return (
-    <Container>
+    <Container $isLoaded={isLoaded}>
       <Banner>
         <ProfComponent />
       </Banner>
@@ -24,7 +39,16 @@ function App() {
 
 export default App;
 
-const Container = styled.div``;
+const fadeIn = keyframes`
+  from { opacity: 0; }
+  to { opacity: 1; }
+`;
+
+const Container = styled.div`
+  opacity: ${props => props.$isLoaded ? 1 : 0};
+  animation: ${props => props.$isLoaded ? fadeIn : 'none'} 0.8s ease-in;
+`;
+
 const Banner = styled.div`
   background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
   height: 50vh;
